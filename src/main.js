08d2c071d5be9e176e48d3ee211eefb1d3f2e5e0
kkazum <49956319+kkazum@users.noreply.github.com>
2021-01-08 02:10:08 +0900

    install electron

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    width: 800,
+    height: 600,
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
